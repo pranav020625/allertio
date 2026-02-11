9cce7ee6ccea387a5fffe7908fbb98d29b587568
PRANAV CH <chittiboyinasriharipranav@gmail.com>
2026-02-11 20:06:26 +0530

    2/11/26

--- test2.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,request,jsonify
-from flask_cors import CORS
-from threading import Thread
-import time
-import smtplib
-from email.mime.text import MIMEText
-from selenium import webdriver
-from selenium.webdriver.chrome
-
